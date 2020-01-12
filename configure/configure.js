78d4be5b7a67e4cd349f03ca50f9cdaf41742c67
Ainul-MdRazib <ainul_mardhiyyah@mymail.sutd.edu.sg>
2020-01-12 20:49:50 +0800

    Implement saving of time offset in extension storage

--- configure/configure.js
+++ configure/configure.js
@@ -2,18 +2,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     var subuh_alarm_button = document.getElementById('subuh_alarm_button');
-    chrome.storage.sync.get("subuh_alarm", function (result) {
-        if (result.subuh_alarm == false) {
-            subuh_alarm_button.style.backgroundImage = "url('../assets/images/alarm_off.png')";
-            chrome.storage.sync.set({ subuh_alarm: false }, function () {
-                console.log('subuh_alarm_setting initialised to ' + false);
-            });
-        } else {
-            subuh_alarm_button.style.backgroundImage = "url('../assets/images/alarm_on.png')";
-            chrome.storage.sync.set({ subuh_alarm: true }, function () {
-                console.log('subuh_alarm_setting initialised to' + true);
-            })
+    var subuh_offset_button = document.getElementById('subuh-offset-button');
+    var subuh_offset_field = document.getElementById('subuh-offset-field');
 
+
+    chrome.storage.sync.get(null, function (result) {
+        if (result.subuh_alarm == true) {
+            subuh_alarm_button.style.backgroundImage = "url('../assets/images/alarm_on.png')";
+            console.log('subuh_alarm_setting initialised to ' + true);
+        } else {
+            subuh_alarm_button.style.backgroundImage = "url('../assets/images/alarm_off.png')";
+            console.log('subuh_alarm_setting initialised to ' + false);
+
+        }
+
+        if (result.subuh_offset != null) {
+            subuh_offset_field.value = result.subuh_offset;
+        } else {
+            subuh_offset_field.value = 0;
         }
 
     });
@@ -29,15 +35,21 @@ document.addEventListener('DOMContentLoaded', function () {
                 subuh_alarm_button.style.backgroundImage = "url('../assets/images/alarm_on.png')";
                 chrome.storage.sync.set({ subuh_alarm: true }, function () {
                     console.log('subuh_alarm_setting is set to ' + true);
-                })
+                });
 
             }
 
-        });
-
-        
+        });  
         
     });
+
+    subuh_offset_button.addEventListener('click', function () {
+        chrome.storage.sync.set({ subuh_offset: subuh_offset_field.value }, function () {
+            console.log("subuh_offset set to " + subuh_offset_field.value);
+        });
+    })
+
+
 
     var save_config_button = document.getElementById('save_config_button');
     save_config_button.addEventListener('click', function () {
