125edd2beadd1510f443b8ee08f722676ae2cc37
Ainul-MdRazib <ainul_mardhiyyah@mymail.sutd.edu.sg>
2020-01-12 16:25:10 +0800

    Complete button click interactions and prayer time row

--- configure/configure.js
+++ configure/configure.js
@@ -1,5 +1,44 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+
+    var subuh_alarm_button = document.getElementById('subuh_alarm_button');
+    chrome.storage.sync.get("subuh_alarm", function (result) {
+        if (result.subuh_alarm == false) {
+            subuh_alarm_button.style.backgroundImage = "url('../assets/images/alarm_off.png')";
+            chrome.storage.sync.set({ subuh_alarm: false }, function () {
+                console.log('subuh_alarm_setting initialised to ' + false);
+            });
+        } else {
+            subuh_alarm_button.style.backgroundImage = "url('../assets/images/alarm_on.png')";
+            chrome.storage.sync.set({ subuh_alarm: true }, function () {
+                console.log('subuh_alarm_setting initialised to' + true);
+            })
+
+        }
+
+    });
+
+    subuh_alarm_button.addEventListener('click', function () {
+        chrome.storage.sync.get("subuh_alarm", function (result) {
+            if (result.subuh_alarm == true) {
+                subuh_alarm_button.style.backgroundImage = "url('../assets/images/alarm_off.png')";
+                chrome.storage.sync.set({ subuh_alarm: false }, function () {
+                    console.log('subuh_alarm_setting is set to ' + false);
+                });
+            } else {
+                subuh_alarm_button.style.backgroundImage = "url('../assets/images/alarm_on.png')";
+                chrome.storage.sync.set({ subuh_alarm: true }, function () {
+                    console.log('subuh_alarm_setting is set to ' + true);
+                })
+
+            }
+
+        });
+
+        
+        
+    });
+
     var save_config_button = document.getElementById('save_config_button');
     save_config_button.addEventListener('click', function () {
         alert("Configuration saved! \nClick on Close Menu to return to the previous page.");
