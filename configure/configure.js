1b89d1d1b4d95925ede81b41368c11e0f730697d
Ainul-MdRazib <ainul_mardhiyyah@mymail.sutd.edu.sg>
2020-02-16 21:59:14 +0800

    Complete layouts for config and popup

--- configure/configure.js
+++ configure/configure.js
@@ -1,92 +1,130 @@
 import { PrayerTimeUpdater } from "./PrayerTimeUpdater.js";
 
-document.addEventListener('DOMContentLoaded', function () {
+const PRAYER_NAMES = ["Fajr", "Syuruk", "Dhuhr", "Asr", "Maghrib", "Isha"];
+const NUM_OF_PRAYERS = 6;
+var ALARM_BUTTON_IDS = [
+  "fajr_alarm_button",
+  "syuruk_alarm_button",
+  "dhuhr_alarm_button",
+  "asr_alarm_button",
+  "maghrib_alarm_button",
+  "isha_alarm_button"
+];
+var ADJUSTED_TIME = [
+  "fajr_adjusted_time",
+  "syuruk_adjusted_time",
+  "dhuhr_adjusted_time",
+  "asr_adjusted_time",
+  "maghrib_adjusted_time",
+  "isha_adjusted_time"
+];
+var OFFSET_FIELDS = [
+  "fajr_offset_field",
+  "syuruk_offset_field",
+  "dhuhr_offset_field",
+  "asr_offset_field",
+  "maghrib_offset_field",
+  "isha_offset_field"
+];
+var OFFSET_BUTTONS = [
+  "fajr_offset_button",
+  "syuruk_offset_button",
+  "dhuhr_offset_button",
+  "asr_offset_button",
+  "maghrib_offset_button",
+  "isha_offset_button"
+];
 
-    var date = new Date();
-    var curr_day = date.getDate();
+function mapIdToDocument(item, index, arr) {
+  arr[index] = document.getElementById(item);
+}
 
-    var prayerTimeUpdater = new PrayerTimeUpdater();
+document.addEventListener("DOMContentLoaded", function() {
+  var date = new Date();
+  var curr_day = date.getDate();
 
-    var fajr_alarm_button = document.getElementById('fajr_alarm_button');
-    var fajr_adjusted_time = document.getElementById('fajr_adjusted_time');
-    var fajr_offset_field = document.getElementById('fajr_offset_field');
-    var fajr_offset_button = document.getElementById('fajr_offset_button');
+  var prayerTimeUpdater = new PrayerTimeUpdater();
 
-    chrome.storage.sync.get(null, function (result) {
-        if (result.fajr_alarm == true) {
-            fajr_alarm_button.style.backgroundImage = "url('../assets/images/alarm_on.png')";
-            console.log('subuh_alarm_setting initialised to ' + true);
-        } else {
-            fajr_alarm_button.style.backgroundImage = "url('../assets/images/alarm_off.png')";
-            console.log('subuh_alarm_setting initialised to ' + false);
+  ALARM_BUTTON_IDS.forEach(mapIdToDocument);
+  ADJUSTED_TIME.forEach(mapIdToDocument);
+  OFFSET_FIELDS.forEach(mapIdToDocument);
+  OFFSET_BUTTONS.forEach(mapIdToDocument);
 
-        }
+  /* 
+  chrome.storage.sync.get(null, function(result) {
+    if (result.fajr_alarm == true) {
+      fajr_alarm_button.style.backgroundImage =
+        "url('../assets/images/alarm_on.png')";
+      console.log("subuh_alarm_setting initialised to " + true);
+    } else {
+      fajr_alarm_button.style.backgroundImage =
+        "url('../assets/images/alarm_off.png')";
+      console.log("subuh_alarm_setting initialised to " + false);
+    }
 
-        fajr_adjusted_time.innerHTML = result.month_data_key[String(curr_day)]["Fajr"];
+    fajr_adjusted_time.innerHTML =
+      result.month_data_key[String(curr_day)]["Fajr"];
 
-        if (result.fajr_offset_key != null) {
-            fajr_offset_field.value = result.fajr_offset_key;
-            
+    if (result.fajr_offset_key != null) {
+      fajr_offset_field.value = result.fajr_offset_key;
+    } else {
+      fajr_offset_field.value = 0;
+    }
+  });
 
-        } else {
-            fajr_offset_field.value = 0;
-        }
-
-    });
-
-    fajr_alarm_button.addEventListener('click', function () {
-        chrome.storage.sync.get("fajr_alarm", function (result) {
-            if (result.fajr_alarm == true) {
-                fajr_alarm_button.style.backgroundImage = "url('../assets/images/alarm_off.png')";
-                chrome.storage.sync.set({ fajr_alarm: false }, function () {
-                    console.log('fajr_alarm_setting is set to ' + false);
-                });
-            } else {
-                fajr_alarm_button.style.backgroundImage = "url('../assets/images/alarm_on.png')";
-                chrome.storage.sync.set({ fajr_alarm: true }, function () {
-                    console.log('fajr_alarm_setting is set to ' + true);
-                });
-
-
-            }
-
-        });  
-        
-    });
-
-    fajr_offset_button.addEventListener('click', function () {
-        chrome.storage.sync.set({ fajr_offset_key: fajr_offset_field.value }, function () {
-            console.log("fajr_offset set to " + fajr_offset_field.value);
+  fajr_alarm_button.addEventListener("click", function() {
+    chrome.storage.sync.get("fajr_alarm", function(result) {
+      if (result.fajr_alarm == true) {
+        fajr_alarm_button.style.backgroundImage =
+          "url('../assets/images/alarm_off.png')";
+        chrome.storage.sync.set({ fajr_alarm: false }, function() {
+          console.log("fajr_alarm_setting is set to " + false);
         });
-
-        prayerTimeUpdater.update_adjusted_time("Fajr");
-
-        chrome.storage.sync.get("month_data_key", function (result) {
-            console.log(result.month_data_key[String(curr_day)]["Fajr"]);
-            console.log("curr_Day is: " + String(curr_day) + "for fajr time: " + result.month_data_key[String(curr_day)]["Fajr"]);
-            fajr_adjusted_time.innerHTML = result.month_data_key[String(curr_day)]["Fajr"];
-
+      } else {
+        fajr_alarm_button.style.backgroundImage =
+          "url('../assets/images/alarm_on.png')";
+        chrome.storage.sync.set({ fajr_alarm: true }, function() {
+          console.log("fajr_alarm_setting is set to " + true);
         });
-
-
-
-
-        
-    })
-
-
-
-
-    var save_config_button = document.getElementById('save_config_button');
-    save_config_button.addEventListener('click', function () {
-        alert("Configuration saved! \nClick on Close Menu to return to the previous page.");
-        //document.getElementById('prayer_times_table').disabled = true;
+      }
     });
+  });
 
-    var close_menu_button = document.getElementById('close_menu_button');
-    close_menu_button.addEventListener('click', function () {
-        chrome.tabs.getCurrent(function (tab) {
-            chrome.tabs.remove(tab.id, function () { });
-        });
+  fajr_offset_button.addEventListener("click", function() {
+    chrome.storage.sync.set(
+      { fajr_offset_key: fajr_offset_field.value },
+      function() {
+        console.log("fajr_offset set to " + fajr_offset_field.value);
+      }
+    );
+
+    prayerTimeUpdater.update_adjusted_time("Fajr");
+
+    chrome.storage.sync.get("month_data_key", function(result) {
+      console.log(result.month_data_key[String(curr_day)]["Fajr"]);
+      console.log(
+        "curr_Day is: " +
+          String(curr_day) +
+          "for fajr time: " +
+          result.month_data_key[String(curr_day)]["Fajr"]
+      );
+      fajr_adjusted_time.innerHTML =
+        result.month_data_key[String(curr_day)]["Fajr"];
     });
+  });
+  */
+
+  var save_config_button = document.getElementById("save_config_button");
+  save_config_button.addEventListener("click", function() {
+    alert(
+      "Configuration saved! \nClick on Close Menu to return to the previous page."
+    );
+  });
+
+  var close_menu_button = document.getElementById("close_menu_button");
+  close_menu_button.addEventListener("click", function() {
+    chrome.tabs.getCurrent(function(tab) {
+      chrome.tabs.remove(tab.id, function() {});
+    });
+  });
 });
--- toolbar_popup/toolbar_popup.js
+++ toolbar_popup/toolbar_popup.js
@@ -1,15 +1,29 @@
-function callback() {
-    console.log("configure menu clicked");
-}
+const PRAYER_NAMES = ["Fajr", "Syuruk", "Dhuhr", "Asr", "Maghrib", "Isha"];
+const ALARM_BUTTON_IDS = [
+  "fajr_alarm_button",
+  "syuruk_alarm_button",
+  "dhuhr_alarm_button",
+  "asr_alarm_button",
+  "maghrib_alarm_button",
+  "isha_alarm_button"
+];
+const ADJUSTED_TIME = [
+  "fajr_adjusted_time",
+  "syuruk_adjusted_time",
+  "dhuhr_adjusted_time",
+  "asr_adjusted_time",
+  "maghrib_adjusted_time",
+  "isha_adjusted_time"
+];
 
-document.addEventListener('DOMContentLoaded', function () {
-    var configureButton = document.getElementById('configure-button');
+document.addEventListener("DOMContentLoaded", function() {
+  var configureButton = document.getElementById("configure-button");
 
-    configureButton.addEventListener('click', function () {
-        if (chrome.runtime.openOptionsPage) {
-            chrome.runtime.openOptionsPage(callback);
-        } else {
-            window.open(chrome.runtime.getURL('configure.html'));
-        };
-    });
+  configureButton.addEventListener("click", function() {
+    if (chrome.runtime.openOptionsPage) {
+      chrome.runtime.openOptionsPage(callback);
+    } else {
+      window.open(chrome.runtime.getURL("configure.html"));
+    }
+  });
 });
